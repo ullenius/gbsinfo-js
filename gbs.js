ab3c3f71ba9c008b87355a793f36d9e644e62467
Anosh D. Ullenius <anosh@anosh.se>
2022-07-17 09:54:35 +0200

    Read version, songs, firstsong and load address

--- gbs.js
+++ gbs.js
@@ -20,6 +20,18 @@ function readFile(input) {
         var identifier = readChar(header.slice(0,3));
         console.log(identifier);
 
+        var version = readByte(header.slice(3,4));
+        console.log("version", version);
+
+        var songs = readByte(header.slice(4,5));
+        console.log("no of songs", songs);
+
+        var firstSong = readByte(header.slice(5,6));
+        console.log("first songs", firstSong);
+
+        var loadAddress = readShort(header.slice(6,8));
+        console.log("load address:", loadAddress.toString(16));
+
       //  console.log(view[0]);
       //  console.log(view);
 
@@ -34,4 +46,14 @@ function readChar(buffer) {
         text = text + ch;
     }
     return text;
+}
+
+function readByte(buffer) {
+    var view = new Uint8Array(buffer);
+    return view[0];
+}
+
+function readShort(buffer) {
+    var view = new Uint16Array(buffer);
+    return view[0];
 }
