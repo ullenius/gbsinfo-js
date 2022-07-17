b6026c093715531db67594bcfed64d17ff7c4fc8
Anosh D. Ullenius <anosh@anosh.se>
2022-07-17 09:42:35 +0200

    Helper method for chars

--- gbs.js
+++ gbs.js
@@ -14,23 +14,23 @@ function readFile(input) {
         var wholeFile = reader.result;
         var header = wholeFile.slice(0, 0x70);
 
-        console.log( header );
+      //  console.log( header );
         console.log("byte length", header.byteLength);
 
-        var view = new Uint8Array(header);
         var identifier = readChar(header.slice(0,3));
         console.log(identifier);
 
-        console.log(view[0]);
-        console.log(view);
+      //  console.log(view[0]);
+      //  console.log(view);
 
     };
 }
 
-function readChar(buffer, length) {
+function readChar(buffer) {
+    var view = new Uint8Array(buffer);
     var text = "";
-    for (var i = 0; i < length; i++) {
-        var ch = String.fromCharCode( buffer[i] );
+    for (var i = 0; i < view.length; i++) {
+        var ch = String.fromCharCode( view[i] );
         text = text + ch;
     }
     return text;
